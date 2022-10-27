0aed631d9da1930a2f6854acc4ca3eb2311cfd39
onuegbu22 <onuegbu22@gmail.com>
2022-10-27 02:46:02 -0700

    2-strncpy

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,25 +1,33 @@
 #include "main.h"
 /**
-* _strcat - concatennates two strings 
-* @dest: pointer to the string to be affix upon 
-* @src: the string to affix  
-* Return: pointer to destination string @dest.
-*/
-char *_strcat(char *dest, const char *str)
+ * strcat - Concatenate the string pointed to by @src
+ * @dest: A pointer to the string to be concatenated upon.
+ * @src: The source string to be appended to @dest.
+ *_strcat - concatenate two strings
+ * @dest: the string to append to
+ * @src: the string to append
+ * Description: This function appends the string pointed to by src to the
+ * string pointed to by dest, overwriting the null byte at the end of dest.
+ * Return: A pointer to the destination string @dest.
+ * Return: a pointer to the destination string dest
+ */
+char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j; i = 0;
-	while (dest[i] != '\0')
+	int i, j;
+	i = 0; while (dest[i] != '\0')
 	{
 		i++;
 	}
 	j = 0;
 	while (src[j] != '\0')
 	{
-		dest[i] = src[j];
-		i++;
-		j++;
+	dest[i] = src[j];
+	i++;
+	j++;
+	
 	}
 	dest[i] = '\0';
 	return (dest);
 }
+
+										
