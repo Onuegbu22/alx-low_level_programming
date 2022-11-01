e08f04fd6a15f6ca9bbe270825fe70b3ecf50c1d
onuegbu22 <onuegbu22@gmail.com>
2022-10-31 19:53:20 -0700

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 /**
  *_strchr - locates a character in a string
  *@c: occurrence of the character
@@ -16,5 +17,4 @@ char *_strchr(char *s, char c)
 	}
 	if (c == '\0')
 		return (s);
-	return (NULL);
 }
